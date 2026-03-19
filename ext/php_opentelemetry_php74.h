270dab67e533e991d43baeb64dc5b19f8575e888
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2026-03-19 10:23:46 +0900

    OpenTelemetry Extension for PHP 7.4

---
+++ ext/php_opentelemetry_php74.h
@@ -0,0 +1,25 @@
+#ifndef PHP_OPENTELEMETRY_PHP74_H
+#define PHP_OPENTELEMETRY_PHP74_H
+
+extern zend_module_entry opentelemetry_php74_module_entry;
+#define phpext_opentelemetry_php74_ptr &opentelemetry_php74_module_entry
+
+ZEND_BEGIN_MODULE_GLOBALS(opentelemetry_php74)
+    HashTable *observer_class_lookup;
+    HashTable *observer_function_lookup;
+    HashTable *observer_aggregates;
+    char *conflicts;
+    int disabled;
+ZEND_END_MODULE_GLOBALS(opentelemetry_php74)
+
+ZEND_EXTERN_MODULE_GLOBALS(opentelemetry_php74)
+
+#define OTEL_G(v) ZEND_MODULE_GLOBALS_ACCESSOR(opentelemetry_php74, v)
+
+#define PHP_OPENTELEMETRY_PHP74_VERSION "0.1.0"
+
+#if defined(ZTS) && defined(COMPILE_DL_OPENTELEMETRY_PHP74)
+ZEND_TSRMLS_CACHE_EXTERN()
+#endif
+
+#endif /* PHP_OPENTELEMETRY_PHP74_H */
