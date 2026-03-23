1fc2e08feafbae9e992fe06a722a50984f0d2783
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2026-03-23 16:28:58 +0900

    refine composer.json

--- src/Instrumentation/Laravel/src/LaravelInstrumentation.php
+++ src/Instrumentation/Laravel/src/LaravelInstrumentation.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Based on code from opentelemetry/opentelemetry-php-contrib
  * Copyright 2021 opentelemetry-php-contrib contributors
@@ -9,8 +11,6 @@
  * - Added support for PHP 7.4
  * - Updated to use OpenTelemetry extension for PHP 7.4
  */
-
-declare(strict_types=1);
 
 namespace OpenTelemetryPHP74\Instrumentation\Laravel;
 
